dfa61d7991ced869704d491738c89bc11be5f4fe
Phil Brown <pdbrown.git@gmail.com>
2024-05-22 14:28:27 -0700

    Add license

--- tests/main/test_byte_seqs.py
+++ tests/main/test_byte_seqs.py
@@ -1,3 +1,6 @@
+# Copyright: (c) 2024, Philip Brown
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 import pytest
 
 from triss.byte_seqs import take_and_drop, resize_seqs
