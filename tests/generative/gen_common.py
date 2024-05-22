e1b5ec98881f710e834a2741174d790c015338a0
Phil Brown <pdbrown.git@gmail.com>
2024-05-22 14:28:39 -0700

    fix comment

--- tests/generative/gen_common.py
+++ tests/generative/gen_common.py
@@ -4,7 +4,7 @@
 from hypothesis import settings
 
 
-# Increase default deadline from 200ms -> 1000ms.
+# Increase default deadline from 200ms -> 5000ms.
 # The deadline applies to a single example. A test will fail if examples
 # consistently take longer than the deadline.
 settings.register_profile("default", deadline=5000)
